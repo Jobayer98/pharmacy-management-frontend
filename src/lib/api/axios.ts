7dbbe2ccd30f26eb57569d697cb999cab17c976e
jobayer <jobayerrahman0220@gmail.com>
2025-11-20 12:50:39 +0600

    add api public url

--- src/lib/api/axios.ts
+++ src/lib/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useUserStore } from "@/store/useUserStore";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000", // your FastAPI base URL
+    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
     withCredentials: true,
 });
 
